8d67d79ffd7024b051746fa530698b62c910595c
Vlad <vladislavmoiseenkov@gmail.com>
2019-06-03 11:11:08 +0300

    change api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:3000',
+  baseURL: 'https://my-kanban-server.herokuapp.com',
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
     'Accept': 'application/json'
